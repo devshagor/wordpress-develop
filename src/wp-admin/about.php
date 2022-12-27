8a673b1f6494f1045457aa695cc808f0c3262aba
Shagor Ahmed <shagors333@gmail.com>
2022-12-27 23:05:33 +0600

    use aligncenter class in about page sub-heading
    on about.php heading was center aligned and the sub-heading is left aligned it's looking odd if we do both centers align it will look better just add the "aligncenter" class in the sub-heading tag in about.php

--- src/wp-admin/about.php
+++ src/wp-admin/about.php
@@ -53,7 +53,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					);
 					?>
 				</h2>
-				<p class="is-subheading">
+				<p class="is-subheading aligncenter">
 					<?php _e( 'This page highlights some of the most significant changes to the product since the May 2022 release of WordPress 6.0. You will also find resources for developers and anyone seeking a deeper understanding of WordPress.' ); ?>
 				</p>
 			</div>
